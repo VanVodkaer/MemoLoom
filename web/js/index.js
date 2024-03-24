85cf4d486da5d7b1158a77a8c0dd5e47b1838d7f
VanVodkaer <mail@vodkaer.com>
2024-03-24 16:59:47 +0800

    更新了readme文件

--- web/js/index.js
+++ web/js/index.js
@@ -1,32 +1,54 @@
 import { config } from "./config.js";
 
 function renderElements(index, title, create = " ", edit = " ") {
-  //外框
   const appendElement = document.createElement("div");
   appendElement.classList.add("note-list-body-box");
   appendElement.id = "li" + index;
 
-  //复选框外框
   const listselectElement = document.createElement("div");
-  listselectElement.classList.add("note-list-body list-select");
-  listselectElement.innerText = title;
+  listselectElement.classList.add("note-list-body", "list-select");
 
-  //复选框
   const checkboxElement = document.createElement("input");
+  checkboxElement.type = "checkbox";
   checkboxElement.classList.add("list-select-btn");
   checkboxElement.id = "ch" + index;
-  checkboxElement.type = "checkbox";
 
-  //标题
-  const selectElement = document.createElement("div");
-  selectElement.classList.add("note-list-body list-note-title");
-  checkboxElement.id = "ti" + index;
-  selectElement.innerText = title;
+  const titleElement = document.createElement("div");
+  titleElement.classList.add("note-list-body", "list-note-title");
+  titleElement.id = "ti" + index;
+  titleElement.textContent = title;
 
-  //创建日期
   const createdateElement = document.createElement("div");
-  createdateElement.classList.add("note-list-body list-create-date");
-  createdateElement.innerText = title;
+  createdateElement.classList.add("note-list-body", "list-create-date");
+  createdateElement.textContent = create;
+
+  const editdateElement = document.createElement("div");
+  editdateElement.classList.add("note-list-body", "list-last-edited");
+  editdateElement.textContent = edit;
+
+  const functionElement = document.createElement("div");
+  functionElement.classList.add("note-list-body", "list-function-btn");
+
+  const editbtnElement = document.createElement("button");
+  editbtnElement.textContent = "编辑";
+  const copybtnElement = document.createElement("button");
+  copybtnElement.textContent = "复制";
+  const delbtnElement = document.createElement("button");
+  delbtnElement.textContent = "删除";
+
+  listselectElement.appendChild(checkboxElement);
+
+  functionElement.appendChild(editbtnElement);
+  functionElement.appendChild(copybtnElement);
+  functionElement.appendChild(delbtnElement);
+
+  appendElement.appendChild(listselectElement);
+  appendElement.appendChild(titleElement);
+  appendElement.appendChild(createdateElement);
+  appendElement.appendChild(editdateElement);
+  appendElement.appendChild(functionElement);
+
+  return appendElement;
 }
 
 const notedataXHR = new XMLHttpRequest();
@@ -36,8 +58,10 @@ notedataXHR.addEventListener("loadend", () => {
   let notedata = JSON.parse(notedataXHR.response);
   // 2. 渲染笔记列表
   // 在nav后面追加
-  const note_list_head_box = document.querySelector(".note-list-head-box");
-  notedata.title.forEach((title, index) => {});
+  const insertplace = document.querySelector(".main-content");
+  notedata.title.forEach((title, index) => {
+    insertplace.appendChild(renderElements(index, title));
+  });
 });
 
 notedataXHR.send();
